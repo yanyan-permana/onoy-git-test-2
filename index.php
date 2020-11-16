82c2977159e5347ef4989c237ce04ceea98420b3
yanyan-permana <deyanyan6@gmail.com>
2020-11-17 05:57:28 +0700

    menambahkan paragrap di file index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <body>
 
 	<h1>Hello World</h1>
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, repellendus sint ullam animi cumque ipsam velit qui, voluptates at architecto facere aut autem itaque soluta ratione id, sapiente. Ipsa, omnis!</p>
 
 </body>
 </html>
